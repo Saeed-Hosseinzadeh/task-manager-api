23c2b453445d400e2a59cd684644197f2552128c
Saeed-Hosseinzadeh <Saeed138173@gmail.com>
2026-03-28 14:10:00 +0330

    add routes debug test

--- app/core/security.py
+++ app/core/security.py
@@ -19,7 +19,7 @@ def hash_password(password: str) -> str:
     """
     Hash a plain password using bcrypt.
     """
-    return pwd_context.hash(password)
+    return pwd_context.hash(password[:72])
 
 
 def verify_password(password: str, hashed: str) -> bool:
